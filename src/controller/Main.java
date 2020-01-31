12853b6dbc75488665f12acf2b661d69699320b7
Kamil Ochnik <ilmak@o2.pl>
2020-01-31 12:27:01 +0100

    first commit

---
+++ src/controller/Main.java
@@ -0,0 +1,51 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        mainWindow();
+    }
+
+    public void mainWindow() {
+        try {
+            FXMLLoader loader = new
+                    FXMLLoader(Main.class.getResource("/view/MainWindowView.fxml"));
+
+            primaryStage.setTitle("Praca domowa nr. 4");
+
+            StackPane pane = loader.load();
+
+            Scene scene = new Scene(pane);
+
+            MainWindowController mainWindowController =
+                    loader.getController();
+
+            mainWindowController.setMain(this, primaryStage);
+            primaryStage.setMinWidth(1000);
+            primaryStage.setMinHeight(600);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/controller/MainWindowController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class MainWindowController {
+    private Main main;
+    private Stage primaryStage;
+
+
+    public void setMain(Main main, Stage primaryStage) {
+        this.main = main;
+        this.primaryStage = primaryStage;
+       // tableView.setItems(employeesList);
+
+    }
+}
