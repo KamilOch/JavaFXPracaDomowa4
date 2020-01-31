6d5b82e16f3a71653937414278fae643a42165ad
Kamil Ochnik <ilmak@o2.pl>
2020-01-31 13:34:28 +0100

    added animation for Ellipse ( basic PathTransition)

--- src/controller/MainWindowController.java
+++ src/controller/MainWindowController.java
@@ -3,8 +3,10 @@ package controller;
 import javafx.animation.Animation;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Path;
 import javafx.stage.Stage;
-import javafx.animation.TranslateTransition;
+//import javafx.animation.TranslateTransition;
+import javafx.animation.PathTransition;
 import javafx.util.Duration;
 
 public class MainWindowController {
@@ -13,18 +15,30 @@ public class MainWindowController {
 
     @FXML
     private Ellipse ellipse;
+    @FXML
+    private Path path;
 
     public void initialize() {
 
-        TranslateTransition translateTransition = new TranslateTransition();
-        translateTransition.setDuration(Duration.seconds(4));
+//        TranslateTransition translateTransition = new TranslateTransition();
+//        translateTransition.setDuration(Duration.seconds(4));
+//
+//        translateTransition.setToY(300);
+//        translateTransition.setToX(150);
+//        translateTransition.setAutoReverse(true);
+//        translateTransition.setCycleCount(Animation.INDEFINITE);
+//        translateTransition.setNode(ellipse);
+//        translateTransition.play();
 
-        translateTransition.setToY(300);
-        translateTransition.setToX(150);
-        translateTransition.setAutoReverse(true);
-        translateTransition.setCycleCount(Animation.INDEFINITE);
-        translateTransition.setNode(ellipse);
-        translateTransition.play();
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.seconds(9));
+
+        pathTransition.setPath(path);
+
+        pathTransition.setAutoReverse(true);
+        pathTransition.setCycleCount(Animation.INDEFINITE);
+        pathTransition.setNode(ellipse);
+        pathTransition.play();
     }
 
     public void setMain(Main main, Stage primaryStage) {
